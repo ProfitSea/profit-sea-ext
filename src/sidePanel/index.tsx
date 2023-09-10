d4adc9fd8d59932e3180d779b631bda6a34096cb
Abdul Quddous <quddoux112@gmail.com>
2023-09-10 16:44:24 +0500

    added loader for styles .css in webpack

--- src/sidePanel/index.tsx
+++ src/sidePanel/index.tsx
@@ -2,6 +2,11 @@ import Button from "@mui/material/Button";
 import React from "react";
 import { createRoot } from "react-dom/client";
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 const SidePanel = () => {
   return (
     <div>
